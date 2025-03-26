8b510e1d63ab4d99196bc7440199c7fcf2cb1b9d
Bronny <bronnyhamilton@gmail.com>
2025-03-27 01:42:31 +1100

    added redirect to home button as component and to sign up page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { SignUpForm } from "../components/SignUpForm";
+import { RedirectHomeButton } from "../components/RedirectHomeButton";
 
 export function SignUp() {
     return (
         <div>
             <h1>Sign Up</h1>
             <SignUpForm />
+
+            <RedirectHomeButton />
         </div>
     );
 }
